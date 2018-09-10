8702b0a8411ab9c259e93ee7c900429818fe4b23
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-09-10 08:23:43 +0300

    testattu TypeScript-tiedoston automaattinen kääntäminen JavaScript-tiedostoksi

---
+++ JavaScriptMuuttujaTesti/JavaScriptMuuttujaTesti/Scripts/Testi3.ts
@@ -0,0 +1,16 @@
+﻿//luokkamääritys TypeScriptillä
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + "" + middleInitial + "" + lastName;
+    }
+}
+interface IPerson {
+    firstName: string;
+    lastName: string;
+}
+function greeter(person: IPerson) {
+    return "Hello, " + person.firstName + "" + person.lastName;
+}
+var user = new Student("Olli", "K.", "Oppilas");
+console.log(greeter(user));
